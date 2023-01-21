103c6b015df09e0d59c96a194eac191a8adc485c
Tomirios2005 <71154189+Tomirios2005@users.noreply.github.com>
2023-01-21 18:26:56 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+document.getElementById('estudios').onclick = function(){
+    document.getElementById('estudio').style.display="block";
+
+}
+document.getElementById('proyectos').addEventListener('click',function(){
+document.getElementById('proyecto').style.display="block"
+})
+
+
+
+
+
+
+    
