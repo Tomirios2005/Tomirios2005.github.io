3ef6c2feb936548c103f571428fb4c4602622fbc
Tomirios2005 <71154189+Tomirios2005@users.noreply.github.com>
2023-01-26 00:32:01 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
-document.getElementById('estudios').onclick = function(){
-    document.getElementById('estudio').style.display="block";
+let estudio = document.getElementById('estudio');
+let proyecto = document.getElementById('proyecto');
+document.getElementById('estudios').addEventListener("click", mostrarEstudios())
+
+
+function mostrarEstudios() {
+    if (estudio.style.display == "none") {
+        estudio.style.display = "block";
+
+    }
+    else {
+        estudio.style.display = "none";
+    }
 
 }
-document.getElementById('proyectos').addEventListener('click',function(){
-document.getElementById('proyecto').style.display="block"
-})
 
 
+document.getElementById('proyectos').addEventListener('click',mostrarProyectos())
+
+function mostrarProyectos() {
+    
+
+    if (proyecto.style.display=="none") {
+        proyecto.style.display="block";
+    }
+    else{
+        proyecto.style.display="none";
+    }
+
+}
 
 
 
